0a42dc98b10e7f57567f1320e2f632367d6a82a3
dzhelezov <dzhelezov@gmail.com>
2022-07-04 19:53:13 +0200

    Introduce Firesquid (#23)
    * introduce firesquid
    
    * typo fix

--- acala/gen-types.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const { typesBundleForPolkadot } = require('@acala-network/type-definitions')
-
-fs.writeFileSync('typesBundle.json', `${JSON.stringify(
-  {
-    spec: typesBundleForPolkadot.spec
-  }, null, 2)}`)
--- crust/gen-types.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-const { typesBundleForPolkadot } = require('@crustio/type-definitions');
-
-fs.writeFileSync('typesBundle.json', JSON.stringify(typesBundleForPolkadot, null, 2));
